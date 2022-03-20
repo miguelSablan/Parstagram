4df1ae425ad712952ae8cf3bc1d3627fa928bc7e
MIGUEL <msabl005@fiu.edu>
2022-03-19 23:25:15 -0400

    Part 2

--- CameraViewController.swift
+++ CameraViewController.swift
@@ -59,21 +59,10 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         let image = info[.editedImage] as! UIImage
         
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af.imageScaled(to: size)
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
 
         imageView.image = scaledImage
         
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
